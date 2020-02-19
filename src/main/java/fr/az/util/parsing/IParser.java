31787773684d4f3cfeed2473f0ec7ba7ff9ab1ac
AZ <35401882+AZ-0@users.noreply.github.com>
2020-02-19 19:47:17 +0100

    Add IParser#safeParse

--- src/main/java/fr/az/util/parsing/IParser.java
+++ src/main/java/fr/az/util/parsing/IParser.java
@@ -14,12 +14,30 @@ import java.util.function.Function;
 public interface IParser<I, O, T extends Throwable> extends Function<I, O>, Serializable
 {
 	/**
-	 * Parse an I into an O
-	 * @param from the provided I
-	 * @return the parsed O
-	 * @throws ParsingException if an error occured while parsing
+	 * Parse an I object into an O object.
+	 * @param from the provided I object.
+	 * @return the parsed O object.
+	 * @throws T if an error occured while parsing.
 	 */
 	O parse(I from) throws T;
+
+	/**
+	 * Parse an I object into an O object, and wraps any occuring {@linkplain Throwable} into a {@linkplain ParsingException}.
+	 * @param from the provided I object.
+	 * @return the parsed O objecT.
+	 * @throws ParsingException wrap any exception that may occurs while parsing.
+	 * @see #parse(I)
+	 */
+	default O safeParse(I from) throws ParsingException
+	{
+		try
+		{
+			return this.parse(from);
+		} catch (Throwable t)
+		{
+			throw new ParsingException(t);
+		}
+	}
 
 	/**
 	 * This method tries to parse an I into an O but only prints the stack trace in the case an exception occurs.
--- src/main/java/fr/az/util/parsing/ParsingException.java
+++ src/main/java/fr/az/util/parsing/ParsingException.java
@@ -8,5 +8,40 @@ public class ParsingException extends Exception
 {
 	private static final long serialVersionUID = -2248076085412390511L;
 
-	public ParsingException(String reason) { super(reason); }
+	private boolean caughtParsing;
+
+	/**
+	 * Constructor
+	 * @param reason
+	 * @see Throwable#Throwable(Throwable)
+	 */
+	public ParsingException(String reason)
+	{
+		super(reason);
+		this.caughtParsing = true;
+	}
+
+	/**
+	 * Constructor
+	 * @param t the Throwable
+	 * @see Throwable#Throwable(Throwable)
+	 */
+	public ParsingException(Throwable t)
+	{
+		super(t);
+	}
+
+	@Override
+	public Throwable initCause(Throwable throwable)
+	{
+		if (throwable instanceof ParsingException)
+			this.caughtParsing = true;
+
+		return super.initCause(throwable);
+	}
+
+	/**
+	 * @return wether the cause of this exception is a ParsingException, or true if this exception has no cause.
+	 */
+	public boolean hasCaughtParsingException() { return this.caughtParsing; }
 }
