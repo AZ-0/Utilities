5556cb3d04c639c599fa3a0563bde0fa3397aaf8
AZ <35401882+AZ-0@users.noreply.github.com>
2020-06-26 17:06:14 +0200

    Fix enum constant retrieval

--- src/main/java/fr/az/util/parsing/json/keys/types/EnumKey.java
+++ src/main/java/fr/az/util/parsing/json/keys/types/EnumKey.java
@@ -1,5 +1,7 @@
 package fr.az.util.parsing.json.keys.types;
 
+import java.util.function.Predicate;
+
 import fr.az.util.parsing.json.JSONParsingException;
 import fr.az.util.parsing.json.keys.Key;
 
@@ -18,13 +20,10 @@ public interface EnumKey<E extends Enum<E>> extends Key<String, E>
 	default E parse(String name) throws JSONParsingException
 	{
 		E[] array = this.getValues();
+		Predicate<String> matcher = this.ignoreCase() ? name::equalsIgnoreCase : name::equals;
 
 		for (E element : array)
-			if (this.ignoreCase())
-			{
-				if (element.name().equals(name))
-					return element;
-			} else if (element.name().equalsIgnoreCase(name))
+			if (matcher.test(element.name()))
 				return element;
 
 		throw new JSONParsingException(this, "Unknown enum constant: "+ name);
