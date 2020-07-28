81b8492e8052be5baa927541fb21b06156146bb2
AZ <35401882+AZ-0@users.noreply.github.com>
2020-07-28 17:40:41 +0200

    Add more constructors for SimpleEnumKey

--- src/main/java/fr/az/util/parsing/json/keys/commons/SimpleEnumKey.java
+++ src/main/java/fr/az/util/parsing/json/keys/commons/SimpleEnumKey.java
@@ -1,12 +1,14 @@
 package fr.az.util.parsing.json.keys.commons;
 
+import java.util.function.Supplier;
+
 import fr.az.util.parsing.json.keys.types.EnumKey;
 
 public class SimpleEnumKey<E extends Enum<E>> implements EnumKey<E>
 {
 	private static final long serialVersionUID = -8241732881464993850L;
 
-	private final Class<E> clazz;
+	private final Supplier<E[]> values;
 	private final String name;
 	private final boolean ignoreCase;
 
@@ -17,13 +19,33 @@ public class SimpleEnumKey<E extends Enum<E>> implements EnumKey<E>
 
 	public SimpleEnumKey(Class<E> enumClass, String name, boolean ignoreCase)
 	{
-		this.clazz = enumClass;
+		this(enumClass::getEnumConstants, name, ignoreCase);
+	}
+
+	public SimpleEnumKey(E[] values, String name)
+	{
+		this(values, name, true);
+	}
+
+	public SimpleEnumKey(E[] values, String name, boolean ignoreCase)
+	{
+		this(() -> values, name, ignoreCase);
+	}
+
+	public SimpleEnumKey(Supplier<E[]> values, String name)
+	{
+		this(values, name, true);
+	}
+
+	public SimpleEnumKey(Supplier<E[]> values, String name, boolean ignoreCase)
+	{
+		this.values = values;
 		this.name = name;
 		this.ignoreCase = ignoreCase;
 	}
 
 	@Override public boolean ignoreCase() { return this.ignoreCase; }
 	@Override public String name(E element) { return element.name(); }
-	@Override public E[] getValues() { return this.clazz.getEnumConstants(); }
+	@Override public E[] getValues() { return this.values.get(); }
 	@Override public String getKey() { return this.name; }
 }
